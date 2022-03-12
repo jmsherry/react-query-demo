959d3d2c05ebd8032f7ed4252b374e642a849c72
James Sherry <james.m.sherry@googlemail.com>
2022-03-12 06:41:52 +0000

    styling

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -34,9 +34,15 @@ function List() {
       {cars.map((car) => (
         <li key={car._id}>
           <img src={car.avatar_url} alt="" className="avatar" />
-          <p className="details">{car.name} ({car.bhp})</p>
-          <Link to={`/update-car/${car._id}`} className="update-link">Update</Link>
-          <button onClick={() => deleteCar(car._id)} className="delete-btn">Delete</button>
+          <p className="details">
+            {car.name} ({car.bhp})
+          </p>
+          <Link to={`/update-car/${car._id}`} className="update-link">
+            Update
+          </Link>
+          <button onClick={() => deleteCar(car._id)} className="delete-btn">
+            Delete
+          </button>
         </li>
       ))}
     </ListStyles>
@@ -49,56 +55,65 @@ const ListStyles = styled.ul`
   width: 80%;
   margin: auto;
 
-  .details {
-    font-weight: bold;
-    font-size: 2rem;
+  & > li {
+    padding: 15px;
+    border: 2px solid hsl(0, 0%, 0%);
+    border-radius: 5px;
+    margin-block-end: 15px;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
   }
 
-& > li {
-  padding: 15px;
-  border: 2px solid #000;
-  border-radius: 5px;
-  margin-block-end: 15px;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-}
+  .details {
+    font-weight: bold;
+    font-size: clamp(1.6rem, 0.5rem + 1vw, 3rem);
+  }
 
-@media screen and (min-width: 768px) {
+  @media screen and (min-width: 768px) {
     display: flex;
     gap: 1em;
     flex-wrap: wrap;
     padding: 0;
 
-  & > li {
-    flex-basis: calc(33.33% - calc(2em / 3));
+    & > li {
+      flex-basis: calc(33.33% - calc(2em / 3));
+    }
   }
-}
 
-.avatar {
-  max-width: 100%;
-  height: auto;
-  aspect-ratio: 1;
-  object-fit: cover;
-  margin-block-end: 1em;
-}
+  .avatar {
+    max-width: 100%;
+    height: auto;
+    aspect-ratio: 1;
+    object-fit: cover;
+    margin-block-end: 1em;
+  }
 
-.delete-btn,
-.update-link {
-  border: 1px solid #000;
-  padding: 5px 10px;
-  background-color: #ff0;
-}
+  .delete-btn,
+  .update-link {
+    border: 1px solid hsl(0, 0%, 0%);
+    padding: 5px 10px;
+    background-color: hsl(60, 100%, 50%);
+  }
 
-.delete-btn {
-  background-color: #f00;
-  border-color: #f00;
-  color: #fff;
-}
-/* 
-.section-heading {
-  margin-inline-start: 15px;
-} */
-`
+  .update-link {
+    &:hover,
+    &:focus {
+      background-color: hsl(60, 100%, 30%);
+    }
+  }
+
+  .delete-btn {
+    background-color: hsl(0, 100%, 50%);
+    border-color: hsl(0, 100%, 50%);
+    color: hsl(0, 0%, 100%);
+
+    &:hover,
+    &:focus {
+      background-color: hsl(0, 100%, 30%);
+    }
+  }
+`;
 
 export default List;
